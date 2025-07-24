2f16b659d6a66319c3c9614044a257f49421b838
Bond-JH <838629462@qq.com>
2025-07-24 17:12:43 +0800

    Initial Commit

---
+++ HappyMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HappyMatch
+//
+//  Created by 杨家豪 on 2025/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HappyMatch/HappyMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyMatchApp.swift
+//  HappyMatch
+//
+//  Created by 杨家豪 on 2025/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HappyMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
